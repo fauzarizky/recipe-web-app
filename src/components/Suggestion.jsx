87b843fcf260c3c06b038236f053e701a1c430bd
fauzarizky <fauzasmg@gmail.com>
2024-02-06 15:09:59 +0700

    add optional chaining

--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -3,11 +3,11 @@ import { Box, List, ListItem } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 export const Suggestion = ({ suggestion }) => {
-  if (suggestion.length === 0) return null;
+  if (suggestion?.length === 0) return null;
   return (
     <Box mt={2} bgColor={"white"} borderRadius={"md"} boxShadow={"md"} position={"absolute"} width={"100%"} zIndex={5}>
       <List>
-        {suggestion.map((menu, i) => (
+        {suggestion?.map((menu, i) => (
           <Link to={`/menu/${menu.strMeal}`} key={i} style={{ textDecoration: "none" }}>
             <ListItem key={i} _hover={{ bgColor: "gray.100" }} cursor={"pointer"}>
               {menu.strMeal}
